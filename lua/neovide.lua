129bc40b487bb8569be7c45f95721f045547e5ae
panr <radoslaw.koziel@gmail.com>
2023-08-20 09:07:13 +0200

    update font for neovide

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -5,3 +5,4 @@ vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true 
 vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
 
+vim.o.guifont = 'Input Mono'
